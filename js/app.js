b916a9e315558453c14540749f001f9f619ca6bd
edocico <138378767+edocico@users.noreply.github.com>
2023-10-17 21:59:54 +0200

    project start

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ console.log('ok')
 const numberDOMElement = document.querySelector('.display-number')
 console.dir(numberDOMElement)
 const playBtnDOMElement = document.getElementById('play-btn')
+const timerDOMElement = document.querySelector('.timer')
+
+let timer = 0
+userNumberArray = []
 
 
 playBtnDOMElement.addEventListener('click', function() {
@@ -12,8 +16,24 @@ playBtnDOMElement.addEventListener('click', function() {
     console.log(randomNumberArray)
     //stampo a schermo i 5 numeri presenti nell'array
     numberDOMElement.innerHTML = randomNumberArray
+    setTimeout(gameStart,10000)
+    setTimeout(askUser, 30000)
+    console.log(userNumberArray)
+
 
 })
+
+function gameStart() {
+    numberDOMElement.classList.add('d-none')
+}
+
+function askUser() {
+    for(let i = 0; i > 5; i++) {
+        const userPrompt = prompt('inserisci un numero')
+        userNumberArray.push(parseInt.userPrompt)
+    }
+}
+
 
 
 // funzione generazione array di numeri casuali
