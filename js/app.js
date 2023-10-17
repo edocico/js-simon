d05c72e06f174913c648d4e88d74df82ab543e34
edocico <138378767+edocico@users.noreply.github.com>
2023-10-17 22:56:49 +0200

    add some logic

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ const timerDOMElement = document.querySelector('.timer')
 
 let timer = 0
 userNumberArray = []
-
+rispArray = []
 
 playBtnDOMElement.addEventListener('click', function() {
     // genero un array di 5 numeri casuali
@@ -16,23 +16,27 @@ playBtnDOMElement.addEventListener('click', function() {
     console.log(randomNumberArray)
     //stampo a schermo i 5 numeri presenti nell'array
     numberDOMElement.innerHTML = randomNumberArray
-    setTimeout(gameStart,10000)
-    setTimeout(askUser, 30000)
-    console.log(userNumberArray)
-
+    // dopo 5 secondi faccio scomparire i numeri
+    setTimeout(function() {
+        numberDOMElement.classList.add('d-none')
+        alert('ora i numeri scompaiono prova a ricordarli')
+    }, 5000)
+    // dopo 30 secondi chiedo con 5 prompt all'utente i numeri che ha visto prima 
+    setTimeout(function() {
+        for(let i = 0; i < 5; i++) {
+            let risposta = parseInt(prompt('inserisci uno per uno i numeri che hai visto prima'))
+            // se il numero della risposta è presente nell'array dei numeri generati lo pusho in un altro array
+            if (userNumberArray.includes(risposta)) {
+                rispArray.push(risposta)
+            }
+        }
+    }, 30000)
+    // in base alla lunghezza dell'array so quanti numeri l'utente ha indovinato
+    console.log(rispArray.length)
 
 })
 
-function gameStart() {
-    numberDOMElement.classList.add('d-none')
-}
 
-function askUser() {
-    for(let i = 0; i > 5; i++) {
-        const userPrompt = prompt('inserisci un numero')
-        userNumberArray.push(parseInt.userPrompt)
-    }
-}
 
 
 
